966e99c47c9bbea69ecc6181e7f992e79ce62f6f
SWAAFIYAH <swaafiyah9@gmail.com>
2024-06-12 16:07:29 +0300

    updates

--- Dom/app/src/main/java/com/example/dom/utils/MainActivity.kt
+++ Dom/app/src/main/java/com/example/dom/utils/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.dom
+package com.example.dom.utils
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dom.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
