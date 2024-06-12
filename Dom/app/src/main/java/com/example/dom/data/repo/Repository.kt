4746552c0f4a464b7235896de98b5c487f7b7a8f
SWAAFIYAH <swaafiyah9@gmail.com>
2024-06-12 16:20:55 +0300

    updates

---
+++ Dom/app/src/main/java/com/example/dom/data/repo/Repository.kt
@@ -0,0 +1,4 @@
+package com.example.dom.data.repo
+
+interface Repository {
+}
---
+++ Dom/app/src/main/java/com/example/dom/data/viewmodels/LandPageViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.dom.data.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class LandPageViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
