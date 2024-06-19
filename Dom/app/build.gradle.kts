821cc8b80a1a2d9db9cf0e094bbf1d7a82a67e85
TerryTess <terrywamboi.ke@gmail.com>
2024-06-19 16:36:46 +0300

    moviepager adapter and HomeFragment

--- Dom/app/build.gradle.kts
+++ Dom/app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.gridlayout)
+    implementation(libs.androidx.databinding.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
