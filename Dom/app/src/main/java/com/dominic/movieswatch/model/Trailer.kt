74f852524c4568b42fda1a1bac77d5329e42b3ac
dominickiplimo <dominickiplimo2017@gmail.com>
2024-06-21 15:56:02 +0300

    MovieDetailsFragment, MoviDetailsViewModel and its navigation

--- Dom/app/src/main/java/com/dominic/movieswatch/model/Trailer.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/model/Trailer.kt
@@ -1,6 +1,9 @@
 package com.dominic.movieswatch.model
 
 data class Trailer(
+    val id: String,
     val key: String,
-    val name: String
+    val name: String,
+    val site: String,
+    val type: String
 )
---
+++ Dom/app/src/main/java/com/dominic/movieswatch/viewmodel/MovieDetailsViewModel.kt
@@ -0,0 +1,47 @@
+package com.dominic.movieswatch.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dominic.movieswatch.model.Movie
+import com.dominic.movieswatch.repository.MovieRepository
+import kotlinx.coroutines.launch
+
+class MovieDetailsViewModel(private val repository: MovieRepository) : ViewModel() {
+
+    private val _movieDetails = MutableLiveData<Movie>()
+    val movieDetails: LiveData<Movie> get() = _movieDetails
+
+    private val _isFavorite = MutableLiveData<Boolean>()
+    val isFavorite: LiveData<Boolean> get() = _isFavorite
+
+    fun loadMovieDetails(movieId: Int) {
+        viewModelScope.launch {
+            val movieDetails = repository.getMovieDetails(movieId)
+            _movieDetails.value = movieDetails
+            _isFavorite.value = repository.isFavorite(movieId)
+        }
+    }
+
+    fun toggleFavorite() {
+        _movieDetails.value?.let { movie ->
+            viewModelScope.launch {
+                val newStatus = repository.toggleFavorite(movie.id)
+                _isFavorite.value = newStatus
+            }
+        }
+    }
+
+//    fun loadTrailer(movieId: Int) {
+//        viewModelScope.launch {
+//            val trailer = repository.getMovieTrailer(movieId)
+//            // Assuming you have a method to play the trailer
+//            playTrailer(trailer.url)
+//        }
+//    }
+
+    private fun playTrailer(url: String) {
+        // Logic to play the trailer
+    }
+}
