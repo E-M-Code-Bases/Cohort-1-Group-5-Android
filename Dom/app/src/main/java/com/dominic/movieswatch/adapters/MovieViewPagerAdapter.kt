3afb140fac8cc4e5712cdce36ad0b920d2108854
Mwenda16 <90953721+Mwenda16@users.noreply.github.com>
2024-06-22 13:48:45 +0300

    refactering adaaoter, movieitems,

--- Dom/app/src/main/java/com/dominic/movieswatch/adapters/MovieViewPagerAdapter.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/adapters/MovieViewPagerAdapter.kt
@@ -14,6 +14,7 @@ class MoviePagerAdapter(fragmentActivity: FragmentActivity) :
         return 4
     }
 
+
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> NowPlaying()
