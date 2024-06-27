efbed331661861b5e699b59e0f968ee269ccc7e8
dominickiplimo <dominickiplimo2017@gmail.com>
2024-06-27 13:56:18 +0300

    added movie details functionality. Refactored fragments and movie adapter

--- Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/NowPlaying.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/NowPlaying.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
+import com.dominic.movieswatch.R
 import com.dominic.movieswatch.adapters.MovieAdapter
 import com.dominic.movieswatch.databinding.FragmentNowPlayingBinding
+import com.dominic.movieswatch.model.Movie
 import com.dominic.movieswatch.repository.NowRepo
 import com.dominic.movieswatch.utils.API_KEY
 import com.dominic.movieswatch.utils.PREF
@@ -20,43 +23,40 @@ class NowPlaying : Fragment() {
     private lateinit var binding: FragmentNowPlayingBinding
     private lateinit var movieAdapter: MovieAdapter
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentNowPlayingBinding.inflate(inflater, container, false)
         val sharedPrefs = requireContext().getSharedPreferences(PREF, Context.MODE_PRIVATE)
-
         val apiKey = sharedPrefs.getString(API_KEY, "")!!
-
         val repo = NowRepo(apiKey)
 
         val nowPlayingViewModel: NowPlayingViewModel by viewModels {
             NowPlayingProvider(repo)
-
         }
 
-
-        nowPlayingViewModel.nowPlayingMovies.observe(
-            viewLifecycleOwner
-        ) { nowPlayingMovies ->
-            movieAdapter = MovieAdapter(nowPlayingMovies)
-            binding.nowPlayingRecView.apply {
-                layoutManager = GridLayoutManager(context, 3)
-                adapter = movieAdapter
-
+        movieAdapter = MovieAdapter(emptyList()) { movie ->
+            val bundle = Bundle().apply {
+                putString("movieTitle", movie.title)
             }
+            findNavController().navigate(R.id.action_global_movieDetails, bundle)
+        }
+
+        binding.nowPlayingRecView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = movieAdapter
+        }
+
+        nowPlayingViewModel.nowPlayingMovies.observe(viewLifecycleOwner) { nowPlayingMovies ->
+            movieAdapter.updateMovies(nowPlayingMovies)
         }
 
         return binding.root
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding.nowPlayingRecView.adapter = null
     }
-
-
 }
--- Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/UpcomingMoviesFragment.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/UpcomingMoviesFragment.kt
@@ -2,30 +2,26 @@ package com.dominic.movieswatch.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.dominic.movieswatch.R
 import com.dominic.movieswatch.adapters.MovieAdapter
-import com.dominic.movieswatch.databinding.FragmentNowPlayingBinding
 import com.dominic.movieswatch.databinding.FragmentUpcomingMoviesBinding
-import com.dominic.movieswatch.repository.NowRepo
+import com.dominic.movieswatch.model.Movie
 import com.dominic.movieswatch.repository.UpcomingRepo
 import com.dominic.movieswatch.utils.API_KEY
 import com.dominic.movieswatch.utils.PREF
-import com.dominic.movieswatch.viewmodel.NowPlayingProvider
-import com.dominic.movieswatch.viewmodel.NowPlayingViewModel
 import com.dominic.movieswatch.viewmodel.UpcomingMoviesProvider
 import com.dominic.movieswatch.viewmodel.UpcomingViewModel
-
 
 class UpcomingMoviesFragment : Fragment() {
     private lateinit var binding: FragmentUpcomingMoviesBinding
     private lateinit var upAdapter: MovieAdapter
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,37 +29,34 @@ class UpcomingMoviesFragment : Fragment() {
     ): View {
         binding = FragmentUpcomingMoviesBinding.inflate(inflater, container, false)
         val sharedPrefs = requireContext().getSharedPreferences(PREF, Context.MODE_PRIVATE)
-
         val apiKey = sharedPrefs.getString(API_KEY, "")!!
-
         val repository = UpcomingRepo(apiKey)
 
-         val upcomingViewModel: UpcomingViewModel by viewModels {
+        val upcomingViewModel: UpcomingViewModel by viewModels {
             UpcomingMoviesProvider(repository)
-
         }
 
-        upcomingViewModel.upcomingMovies.observe(
-            viewLifecycleOwner
-        ) { upcomingMovies ->
-            upAdapter = MovieAdapter(upcomingMovies)
-            binding.upcomingRecyclerView.apply {
-                layoutManager = GridLayoutManager(context, 3)
-                adapter =upAdapter
-
+        upAdapter = MovieAdapter(emptyList()) { movie ->
+            val bundle = Bundle().apply {
+                putString("movieTitle", movie.title)
             }
+            findNavController().navigate(R.id.action_global_movieDetails, bundle)
+        }
+
+        binding.upcomingRecyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = upAdapter
+        }
+
+        upcomingViewModel.upcomingMovies.observe(viewLifecycleOwner) { upcomingMovies ->
+            upAdapter.updateMovies(upcomingMovies)
         }
 
         return binding.root
-
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding.upcomingRecyclerView.adapter = null
     }
-
-
-
-
 }
