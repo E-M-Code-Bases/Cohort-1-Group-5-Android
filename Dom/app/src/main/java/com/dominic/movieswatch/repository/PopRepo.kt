927023d5a9216706bd49cd4597d624ddd97adcc8
Baraka <raphaelbaraka424@gmail.com>
2024-06-30 10:46:12 +0300

    appdatabase

--- Dom/app/src/main/java/com/dominic/movieswatch/repository/PopRepo.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/repository/PopRepo.kt
@@ -4,22 +4,13 @@ import com.dominic.movieswatch.di.AppModule
 import com.dominic.movieswatch.model.MoviesResponse
 import retrofit2.Response
 
-/**
- Repository class responsible for fetching popular movies.
- *
- * @param apiKey The API key for accessing the movie service.
- */
 class PopRepo(private val apiKey: String) {
 
     // Initializes Retrofit instance using the provided API key.
     private val appModule = AppModule().getRetrofitInstance(apiKey)
-
-    /**
-     * Fetches a list of popular movies from the API.
-     *
-     * @return A Response object containing MoviesResponse data.
-     */
+   // A Response containing the MoviesResponse data.
     suspend fun getPopularMovies(): Response<MoviesResponse> {
         return appModule.getPopularMovies()
+
     }
 }
