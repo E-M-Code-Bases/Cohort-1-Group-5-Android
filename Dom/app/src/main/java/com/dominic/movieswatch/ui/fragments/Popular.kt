c5218a99654a4edd4ddab78323d58e6966a57a35
Baraka <raphaelbaraka424@gmail.com>
2024-06-30 11:02:39 +0300

    appdatabase

--- Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/Popular.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/Popular.kt
@@ -1,10 +1,11 @@
-package com.example.domflex.ui
+package com.dominic.movieswatch.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -20,32 +21,24 @@ import com.dominic.movieswatch.viewmodel.PopularViewmodel
 
 class Popular : Fragment() {
 
-    // View binding for accessing views in the layout.
     private lateinit var binding: FragmentPopularMoviesBinding
-
-    // Adapter for the RecyclerView to display movie data.
     private lateinit var popAdapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout using view binding.
         binding = FragmentPopularMoviesBinding.inflate(inflater, container, false)
 
-        // Retrieve the API key from shared preferences.
         val sharedPrefs = requireContext().getSharedPreferences(PREF, Context.MODE_PRIVATE)
         val apiKey = sharedPrefs.getString(API_KEY, "")!!
 
-        // Initialize the repository with the API key.
         val popRepo = PopRepo(apiKey)
 
-        // Set up the ViewModel with the repository.
         val popularViewModel: PopularViewmodel by viewModels {
             PopularProvider(popRepo)
         }
 
-        // Initialize the adapter with an empty list and set up item click listener.
         popAdapter = MovieAdapter(emptyList()) { movie ->
             val bundle = Bundle().apply {
                 putString("movieTitle", movie.title)  // Pass the selected movie title to the next screen.
@@ -53,15 +46,15 @@ class Popular : Fragment() {
             findNavController().navigate(R.id.action_global_movieDetails, bundle)
         }
 
-        // Set up the RecyclerView with a grid layout manager and the adapter.
         binding.recyclerViewpopular.apply {
             layoutManager = GridLayoutManager(context, 3)
             adapter = popAdapter
         }
 
-        // Observe popular movies from the ViewModel and update the adapter.
+        // Observe popular movies and update the adapter.
         popularViewModel.popularMovies.observe(viewLifecycleOwner) { popularMovies ->
             popAdapter.updateMovies(popularMovies)
+            Toast.makeText(context, "Loaded ${popularMovies.size} movies", Toast.LENGTH_SHORT).show()
         }
 
         return binding.root
