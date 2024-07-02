96c91bf89e881f1dcde5859984707466e6d4bc0c
dominickiplimo <dominickiplimo2017@gmail.com>
2024-07-02 17:03:59 +0300

    handingle adding and removing favorites

--- Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/MovieDetailsFragment.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/ui/fragments/MovieDetailsFragment.kt
@@ -8,8 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.dominic.movieswatch.R
 import com.dominic.movieswatch.databinding.FragmentMovieDetailsBinding
 import com.dominic.movieswatch.repository.MovieRepository
+import com.dominic.movieswatch.utils.API_KEY
 import com.dominic.movieswatch.viewmodel.MovieDetailsViewModel
 import com.dominic.movieswatch.viewmodel.MovieDetailsViewModelFactory
 
@@ -18,7 +20,7 @@ class MovieDetailsFragment : Fragment() {
     private lateinit var binding: FragmentMovieDetailsBinding
     private val args: MovieDetailsFragmentArgs by navArgs()
     private val viewModel: MovieDetailsViewModel by viewModels {
-        MovieDetailsViewModelFactory(MovieRepository(apikey = String())) // Pass the repository instance
+        MovieDetailsViewModelFactory(MovieRepository(API_KEY))
     }
 
     override fun onCreateView(
@@ -41,7 +43,48 @@ class MovieDetailsFragment : Fragment() {
             }
         }
 
+        viewModel.isFavorite.observe(viewLifecycleOwner) { isFavorite ->
+            val favoriteIconRes = if (isFavorite) R.drawable.ic_favorite else R.drawable.ic_favorite_border
+            binding.favoriteIcon.setImageResource(favoriteIconRes)
+        }
 
         return binding.root
     }
 }
+
+/*class MovieDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentMovieDetailsBinding
+    private val args: MovieDetailsFragmentArgs by navArgs()
+    private val viewModel: MovieDetailsViewModel by viewModels {
+        MovieDetailsViewModelFactory(MovieRepository(API_KEY))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val movieTitle = args.movie.title
+
+        viewModel.getMovieDetails(movieTitle).observe(viewLifecycleOwner) { movie ->
+            movie?.let {
+                binding.movie = it
+                val url = "https://image.tmdb.org/t/p/w500" + it.poster_path
+                Glide.with(this)
+                    .load(url)
+                    .into(binding.moviePoster)
+            }
+        }
+
+        viewModel.isFavorite.observe(viewLifecycleOwner) { isFavorite ->
+            val favoriteIconRes = if (isFavorite) R.drawable.ic_favorite else R.drawable.ic_favorite_border
+            binding.favoriteIcon.setImageResource(favoriteIconRes)
+        }
+
+        return binding.root
+    }
+}*/
