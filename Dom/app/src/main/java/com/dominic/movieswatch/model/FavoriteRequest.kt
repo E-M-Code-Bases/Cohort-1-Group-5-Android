d47b0378e4f5dd1f05f5b96e8bcdc3f4766877d9
dominickiplimo <dominickiplimo2017@gmail.com>
2024-07-02 16:13:51 +0300

    favorite repository methods

--- Dom/app/src/main/java/com/dominic/movieswatch/model/FavoriteRequest.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/model/FavoriteRequest.kt
@@ -1,7 +1,7 @@
 package com.dominic.movieswatch.model
 
 data class FavoriteRequest(
-    val media_type: String,
-    val media_id: Int,
+    val movie_type: String,
+    val movie_id: Int,
     val favorite: Boolean
 )
