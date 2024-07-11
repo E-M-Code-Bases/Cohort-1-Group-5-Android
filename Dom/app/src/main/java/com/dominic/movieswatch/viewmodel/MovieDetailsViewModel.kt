b9d27f8001a8261febb31d99633cafff7cfa72b2
Mwenda16 <90953721+Mwenda16@users.noreply.github.com>
2024-07-11 12:13:56 +0300

    cleanup

--- Dom/app/src/main/java/com/dominic/movieswatch/viewmodel/MovieDetailsViewModel.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/viewmodel/MovieDetailsViewModel.kt
@@ -20,6 +20,7 @@ class MovieDetailsViewModel(private val repository: MovieDetailsRepo) : ViewMode
     private val _isFavorite = MutableLiveData<Boolean>()
     val isFavorite: LiveData<Boolean> get() = _isFavorite
 
+
     fun getMovieDetails(title: String): LiveData<Movie?> {
         viewModelScope.launch {
 
