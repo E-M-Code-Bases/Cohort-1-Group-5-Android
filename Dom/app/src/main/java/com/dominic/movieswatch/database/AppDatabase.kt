d438b39b548ce55c5c11c1794cabfef93bca1e84
dominickiplimo <dominickiplimo2017@gmail.com>
2024-07-12 09:15:17 +0300

    Room db update

--- Dom/app/src/main/java/com/dominic/movieswatch/database/AppDatabase.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/database/AppDatabase.kt
@@ -8,29 +8,26 @@ import com.dominic.movieswatch.model.Movie
 import androidx.room.TypeConverters
 import com.dominic.movieswatch.model.Converters
 
-@Database(entities = [Movie::class], version = 2, exportSchema = false)
-@TypeConverters(Converters::class)
+@Database(entities = [Movie::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
-
     abstract fun movieDao(): MovieDao
 
     companion object {
         @Volatile
-        private var INSTANCE: AppDatabase? = null
+        private var instance: AppDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase {
-
-            return INSTANCE ?: synchronized(this) {
-
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "movie_database"
-                ).build()
-                INSTANCE = instance
-
-                instance
+        fun getDatabase(context: Context): AppDatabase =
+            instance ?: synchronized(this) {
+                instance ?: buildDatabase(context).also {
+                    instance = it
+                }
             }
-        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+                AppDatabase::class.java,
+                "movies.db")
+                .build()
     }
 }
+
--- Dom/app/src/main/java/com/dominic/movieswatch/database/MovieDao.kt
+++ Dom/app/src/main/java/com/dominic/movieswatch/database/MovieDao.kt
@@ -8,22 +8,13 @@ import com.dominic.movieswatch.model.Movie
 
 @Dao
 interface MovieDao {
-    @Query("SELECT * FROM movies WHERE id = 1")
-    fun getFavoriteMovies(): LiveData<List<Movie>>
-
-    @Query("UPDATE movies SET adult = 1 WHERE id = :movieId")
-    suspend fun markAsFavorite(movieId: Int)
-
-    @Query("UPDATE movies SET adult = 0 WHERE id = :movieId")
-    suspend fun removeFromFavorites(movieId: Int)
-
-    @Query("SELECT * FROM movies WHERE overview = 'now_watching'")
-    fun getNowWatchingMovies(): LiveData<List<Movie>>
+    @Query("SELECT * FROM movies")
+    suspend fun getAllMovies(): List<Movie>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addFavoriteMovie(movie: Movie)
+    suspend fun insertMovies(movies: List<Movie>)
 
-    @Delete
-    suspend fun removeFavoriteMovie(movie: Movie)
+    @Query("DELETE FROM movies")
+    suspend fun clearMovies()
 }
 
